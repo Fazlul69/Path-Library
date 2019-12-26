55e84c2faad207cd61e8378bfb05bf3c37d8524b
Fazlul69 <sawravm299@gmail.com>
2019-12-26 12:56:03 +0600

    Initial commit

---
+++ Assets/Scripts/Cube 3D.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using PathCreation;
+
+public class Cube3D : MonoBehaviour
+{
+    public PathCreator myPath;
+    float updatePos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        updatePos += 5 * Time.deltaTime;
+        transform.position = myPath.path.GetPointAtDistance(updatePos);
+        transform.rotation = myPath.path.GetRotationAtDistance(updatePos);
+    }
+}
---
+++ Assets/Scripts/player2d.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using PathCreation;
+
+public class player2d : MonoBehaviour
+{
+    public PathCreator myPath;
+    float updatePos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        updatePos += 5 * Time.deltaTime;
+        transform.position = myPath.path.GetPointAtDistance(updatePos);
+        Quaternion angle = myPath.path.GetRotationAtDistance(updatePos);
+
+        angle.x = 0;
+        angle.y = 0;
+        transform.rotation = angle;
+    }
+}
